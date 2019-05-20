537ade31d829ecfaae4b4aef215f2a7e77f1a5d7
phseven <phseven@users.noreply.github.com>
2019-05-20 00:39:22 -0400

    Updated the analysis result html

--- app/static/client.js
+++ app/static/client.js
@@ -14,7 +14,7 @@ function showPicked(input) {
 
 function analyze() {
     var uploadFiles = el('file-input').files;
-    if (uploadFiles.length != 1) alert('Please select 1 file to analyze!');
+    if (uploadFiles.length != 1) alert('Please select a file to analyze!');
 
     el('analyze-button').innerHTML = 'Analyzing...';
     var xhr = new XMLHttpRequest();
@@ -24,7 +24,7 @@ function analyze() {
     xhr.onload = function(e) {
         if (this.readyState === 4) {
             var response = JSON.parse(e.target.responseText);
-            el('result-label').innerHTML = `Result = ${response['result']}`;
+            el('result-label').innerHTML = `The flower in the photo is ${response['result']}`;
         }
         el('analyze-button').innerHTML = 'Analyze';
     }
