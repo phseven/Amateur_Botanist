52487b06568dc636e7a40f24832302a596efda90
phseven <phseven@users.noreply.github.com>
2019-05-20 01:08:51 -0400

    Fixed typo in title()

--- app/server.py
+++ app/server.py
@@ -55,7 +55,7 @@ async def analyze(request):
     img_bytes = await (data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
-    return JSONResponse({'result': str(prediction).Title()})
+    return JSONResponse({'result': str(prediction).title()})
 
 if __name__ == '__main__':
     if 'serve' in sys.argv: uvicorn.run(app=app, host='0.0.0.0', port=5042)
